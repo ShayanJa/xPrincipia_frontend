78958aba4926de202f18d48175bb26aa2268770b
Shyshawn <shayan.Talebi@xprincipia.com>
2017-03-09 19:53:02 -0600

    Router functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router';
 import './assets/App.css';
 import './assets/Create.css';
 import Header from './components/Header.jsx'
 import Solution from './components/Solution.jsx'
 import Problem from './components/Problem.jsx'
 import Create from './components/Create.jsx'
+
 
 class App extends Component {
   render() {
@@ -21,3 +24,15 @@ class App extends Component {
 }
 
 export default App;
+
+//Routing
+ReactDOM.render((
+   <Router history = {browserHistory}>
+      <Route>
+         <IndexRoute component = {App} />
+         {/*<Route path = "/home" component = {Home} />*/}
+      </Route>
+      
+   </Router>
+	
+), document.getElementById('root'))
