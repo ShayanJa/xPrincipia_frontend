c673ee117d34f1f306382c66c942af2c0f3f1469
Shyshawn <shayan.Talebi@xprincipia.com>
2017-03-10 02:15:05 -0600

    added jquery and axios library, Fixed Ajax post request for solution.

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router';
 import $ from "min-jquery";
-
+import axios from "axios"
+import propTypes from 'react';
+import { apiMiddleware } from 'redux-api-middleware';
 
 export default class Create extends React.Component {
     constructor(){
@@ -21,28 +23,32 @@ export default class Create extends React.Component {
     };
 
     postSolution() {
+        //Read field items into component state
         this.state.title = document.getElementById("solutionTitle").value
         this.state.summary = document.getElementById("solutionSummary").value
         this.state.description = document.getElementById("solutionDescription").value
         this.state.evidence = document.getElementById("solutionEvidence").value
+        
+        //Ajax post solution request
         $.ajax({
             crossDomain:"true",
             type:'POST',
-            contentType: 'application/json',
+            headers: {'Content-Type':'application/json'},
             url: 'http://localhost:10000/solutions/create',
             processData: false,
             data: JSON.stringify({
-                "title":this.state.username,
-                "summary":this.state.password,
+                "title":this.state.title,
+                "summary":this.state.summary,
                 "description":this.state.description,
-                "evidence":this.state.evidence,
+                "evidence":this.state.evidence
             }),
             success: function(result){
                 console.log(result)
-                alert("Solution Posted")
+                
+                alert("Your solution has been posted!")
             },
             error : function(result){
-                alert('Unable to post SOlution :()')
+                alert('Sorry! There was an error with your request, Try again in a few minutes')
             }
         });
     }
@@ -52,17 +58,17 @@ export default class Create extends React.Component {
           <form  >
           <fieldset>
               <legend>Create: Solution</legend>
-                <label for="solutionTitle">Title<br />
-                    <input type="text" id="solutionTitle" equired="required" maxlength="100"/></label><br />
-                <label for="solutionSummary">Summary<br />
-                    <input type="text" id="solutionSummary" required="required" maxlength="500" /></label><br />
-                <label for="solutionDescription">Description<br />
-                    <textarea id="solutionDescription" required="required"></textarea>
+                <label htmlFor="solutionTitle">Title<br />
+                    <input type="text" id="solutionTitle" required /></label><br />
+                <label htmlFor="solutionSummary">Summary<br />
+                    <input type="text" id="solutionSummary" required  /></label><br />
+                <label htmlFor="solutionDescription">Description<br />
+                    <input id="solutionDescription" required></input>
                 </label><br />
-                <label for="solutionEvidence">Evidence/Logic<br />
-                    <textarea id="solutionEvidence"></textarea>
+                <label htmlFor="solutionEvidence">Evidence/Logic<br />
+                    <input id="solutionEvidence"></input>
                 </label><br />
-                 <button type="submit"  onClick={this.postSolution}>Submit</button>
+                 <button type="button" onClick={this.postSolution}>Submit</button>
         </fieldset>
     </form>
 </div>
