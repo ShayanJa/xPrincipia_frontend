452a83f625177507ae7697903477d753fd1c75dc
Shyshawn <shayan.Talebi@xprincipia.com>
2017-03-30 10:04:20 -0500

    Create Solution http request works

--- src/components/CreateSolution.jsx
+++ src/components/CreateSolution.jsx
@@ -27,35 +27,35 @@ export default class CreateSolution extends React.Component {
     this.state.description = document.getElementById('solutionDescriptionForm').value
     this.state.evidence = document.getElementById('solutionEvidenceForm').value
     this.state.experiments = document.getElementById('solutionExperimentsForm').value
-    this.state.references = document.getElementById('solutionReferencesForm').value
+    // this.state.references = document.getElementById('solutionReferencesForm').value
 
   //Ajax post solution request
-  // $.ajax({
-  //   crossDomain: 'true',
-  //   type: 'POST',
-  //   headers: {'Content-Type' : 'application/json'},
-  //   url: 'http://localhost:10000/solutions/create',
-  //   processData: false,
-  //   data: JSON.stringify({
-  //     'title' : this.state.title,
-  //     'summary' : this.state.summary,
-  //     'description' : this.state.description,
-  //     'evidence' : this.state.evidence,
-  //     'experiments' : this.state.experiments,
-  //     'references' : this.state.references
-  //   }),
-  //   success: function(result){
-  //     console.log(result)
-  //
-  //     alert('Your solution has been posted.')
-  //   },
-  //   error: function(result){
-  //     console.log(result)
-  //
-  //     alert('There was an error.')
-  //   },
-  //
-  //   });
+  $.ajax({
+    crossDomain: 'true',
+    type: 'POST',
+    headers: {'Content-Type' : 'application/json'},
+    url: 'http://localhost:10000/solutions/create',
+    processData: false,
+    data: JSON.stringify({
+      'title' : this.state.title,
+      'summary' : this.state.summary,
+      'description' : this.state.description,
+      'evidence' : this.state.evidence,
+      'experiments' : this.state.experiments,
+      'references' : this.state.references
+    }),
+    success: function(result){
+      console.log(result)
+  
+      alert('Your solution has been posted.')
+    },
+    error: function(result){
+      console.log(result)
+  
+      alert('There was an error.')
+    },
+  
+    });
   }
 
   render() {
@@ -65,7 +65,7 @@ export default class CreateSolution extends React.Component {
             <fieldset>
                 <legend>Solution</legend>
                       <label htmlFor="solutionTitle">Title<br />
-                          <input type="text" name="solutionTitle" required="required" maxLength="140" autofocus/>
+                          <input type="text" name="solutionTitle" required="required" maxLength="140" id="solutionTitleForm" autofocus/>
                         </label><br />
 
                       <label htmlFor="solutionSummary">Summary<br />
@@ -81,13 +81,12 @@ export default class CreateSolution extends React.Component {
                           </textarea></label><br />
 
                       <label htmlFor="futureExperiments">Future Experiments<br />
-                          <textarea name="futureExperiments" placeholder="Outline future experiment ideas here." id="futureExperimentsForm">
+                          <textarea name="futureExperiments" placeholder="Outline future experiment ideas here." id="solutionExperimentsForm">
                           </textarea></label><br />
 
                       <label htmlFor="solutionReferences">References<br />
                           <textarea name="solutionReferences" placeholder="Provide your references here." id="solutionReferencesForm">
                           </textarea></label><br />
-
                       <input type="submit" value="Create" onClick={this.postSolution} id="submitSolution"/>
             </fieldset>
           </form>
