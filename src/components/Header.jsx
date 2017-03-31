b14623f1df2f36d82f1b9e9edde35253fc445b3d
bfrance18@gmail.com <bfrance18gmailcom@bfrance18gmailcoms-MacBook-Pro.local>
2017-03-31 00:28:24 -0500

    made welcomecontainer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,21 +6,21 @@ export default class Header extends React.Component {
    render() {
       return (
       <div id="headerSection">
-        <ul id="header">
-            <li id="explore">
+        <div id="header">
+            <div id="explore">
                 <form action="http://www.xprincipia.com/search.php" method="get" id="exploreForm">
                     <input type="search" name="search"
                         placeholder="Explore" id="exploreInput" autofocus/>
                     <input type="submit" value="Go" id="submitExplore" />
                 </form>
-            </li>
-            <li id="logo">
+            </div>
+            <div id="logo">
               <a href="./welcome"><div>XPrincipia</div></a>
-            </li>
-            <li id="avatarHeader">
+            </div>
+            <div id="avatarHeader">
               <img src="../assets/dnablackinvert.png" id="img-rounded" id="avatarButton" width="33" height="33" />
-            </li>
-        </ul>
+            </div>
+        </div>
       </div>
       );
    }
