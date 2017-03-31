150da356c67966a732d9f6115220b4eb3643a2d3
bfrance18@gmail.com <bfrance18gmailcom@bfrance18gmailcoms-MacBook-Pro.local>
2017-03-31 00:08:37 -0500

    cleaning up sidebar imports

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,14 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route, Link, hashHistory, IndexRoute  } from 'react-router';
-import CreateSolution from './CreateSolution.jsx';
-import CreateProblem from './CreateProblem.jsx';
-import SolutionContainer from './SolutionContainer.jsx';
-import SuggestionContainer from './SuggestionContainer.jsx';
-import QuestionContainer from './QuestionContainer.jsx';
 import SideBarMenu from './SideBarMenu.jsx';
-import SideBarMore from './SideBarMore.jsx';
-import AnswerContainer from './AnswerContainer';
 
 
 export default class SideBar extends React.Component {
