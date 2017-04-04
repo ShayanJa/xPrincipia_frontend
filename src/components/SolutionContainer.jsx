938490cbaa07d3807fb0c0cde1135a6ca14d4b0b
Shyshawn <shayan.Talebi@xprincipia.com>
2017-04-04 01:14:06 -0500

    Solutions/all gets all solutions, and solutionUnit display individual unit using axios

--- src/components/SolutionContainer.jsx
+++ src/components/SolutionContainer.jsx
@@ -3,19 +3,36 @@ import ReactDOM from 'react-dom';
 import {Link} from 'react-router';
 import SolutionUnit from './SolutionUnit.jsx';
 import SideBarMore from './SideBarMore.jsx';
+import axios from 'axios'
 
 export default class SolutionContainer extends React.Component {
+  constructor(props){
+        super(props);
+
+        this.state = {
+            solutions: []
+        }
+        
+    };
+        componentWillMount(){
+        var self = this;
+        return axios.get('http://localhost:10000/solutions/all').then(function (response) {
+            console.log(response.data[0].Title)
+            self.setState({
+                solutions: response.data
+            })
+        })  
+    }
    render() {
       return (
         <div id="solutions">
           <Link to="/problem/createsolution"><div id="createButton">Create</div></Link>
-          <SolutionUnit />
-          <SolutionUnit />
-          <SolutionUnit />
-          <SolutionUnit />
-          <SolutionUnit />
+            <SolutionUnit solutions={this.state.solutions} />
           <SideBarMore />
         </div>
+      
       );
    }
 }
+
+
--- src/components/SolutionUnit.jsx
+++ src/components/SolutionUnit.jsx
@@ -1,21 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {Link} from 'react-router';
 export default class SolutionUnit extends React.Component {
+	constructor(props){
+        super(props);
+	}
 	render() {
 		return (
 
 	    <div id="solutionUnit">
-	        <div id="solutionUnitTitle">
+	        {/*<div id="solutionUnitTitle">
 	            <div id="percent">70%</div>
-	            <div id="unitTitle">Tryptophan Focused Orchestrated Objective Reduction</div>
+	            <div id="unitTitle">{this.props.solutions}</div>
 	        </div>
-	            <div id="solutionUnitSummary">
-	               Based on Orch OR model. Quantum computation occurs between entangled
-								 states in Tryptophan. Model offers experimentation with this in mind.
+	            <div id="solutionUnitSummary">*/}
+					<ul> {this.props.solutions.map(this.renderItem)} </ul>
+	               
 	            </div>
-
-	    </div>
+	    // </div>
 		);
 	}
+	renderItem(solution) {
+  
+    return (
+        <li key={solution.ID}>
+        	<Link to={{pathname: '/solutions/'+solution.ID }} >
+				<div id="solutionUnitTitle">
+					<div id="percent">70%</div>
+					<div id="unitTitle">{solution.Title}</div>
+				</div>
+				<div id="solutionUnitSummary">
+					{solution.Summary}
+				</div>
+			</Link>
+
+        <br ></br> 
+        </li>)
+  }
 }
+
