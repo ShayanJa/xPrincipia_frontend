11ddd5f6adb74358b7598c9a1ba2249e4cbf5ba0
Shyshawn <shayan.Talebi@xprincipia.com>
2017-04-04 23:32:37 -0500

    fixed suggestions api call

--- src/components/SuggestionContainer.jsx
+++ src/components/SuggestionContainer.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import SuggestionUnit from './SuggestionUnit.jsx';
 import SuggestionForm from './SuggestionForm.jsx';
 import SideBarMore from './SideBarMore.jsx';
 
+
 export default class SuggestionContainer extends React.Component {
+constructor(props){
+        super(props);
+
+        this.state = {
+            suggestions: []
+        }
+        
+    };
+        componentWillMount(){
+        var self = this;
+        return axios.get('http://localhost:10000/suggestions/all').then(function (response) {
+            console.log(response.data[0].Title)
+            self.setState({
+                suggestions: response.data
+            })
+        })  
+    }
    render() {
       return (
         <div id="suggestionContainer">
             <SuggestionForm />
-            <SuggestionUnit />
-            <SuggestionUnit />
-            <SuggestionUnit />
-            <SuggestionUnit />
-            <SuggestionUnit />
+            <SuggestionUnit suggestions={this.state.suggestions} />
             <SideBarMore />
         </div>
       );
