14310425406279985460ba6cbae4463a2a213693
Shyshawn <shayan.Talebi@xprincipia.com>
2017-04-07 01:36:09 -0500

    fix small merge error

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import './assets/App.css';
 import './assets/index.css';
-// import Header from './components/Header.jsx';
-// import FullProblem from './components/FullProblem.jsx';
-// import SideBarProblem from './components/SideBarProblem.jsx';
-// import SolutionForm from './components/SolutionForm.jsx';
-// import SolutionContainer from './components/SolutionContainer.jsx';
-// import QuestionContainer from './components/QuestionContainer.jsx';
-// import SubProblemContainer from './components/SubProblemContainer.jsx';
-// import SuggestionContainer from './components/SuggestionContainer.jsx';
-// import SideBarProblemMenu from './components/SideBarProblemMenu.jsx';
-
 
 
 class App extends React.Component {
