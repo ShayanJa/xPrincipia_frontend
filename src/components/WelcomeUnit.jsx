2388bf48ecb27ede20d7370de6367d336c2ea7f4
bfrance18@gmail.com <bfrance18gmailcom@bfrance18gmailcoms-MacBook-Pro.local>
2017-04-21 02:09:02 -0500

    changed link to being outside of list element in welcome unit, so you can click anywhere in its box, not just its name

--- src/components/WelcomeUnit.jsx
+++ src/components/WelcomeUnit.jsx
@@ -15,17 +15,18 @@ export default class WelcomeUnit extends React.Component {
 	renderItem(problem) {
   
       return (
-
-      <li key={problem.ID} id="welcomeProblemsUnit">
-          <Link to={{pathname: '/problem/'+problem.ID +'/subproblems'}}>
-            <div id="welcomeProblemsHeader">
-              <div id="welcomeProblemsTag">{problem.Field}</div>
-              <div id="welcomeProblemsTitle">
-                  {problem.Title}
+      <Link to={{pathname: '/problem/'+problem.ID +'/subproblems'}}>
+        <li key={problem.ID} id="welcomeProblemsUnit">
+            
+              <div id="welcomeProblemsHeader">
+                <div id="welcomeProblemsTag">{problem.Field}</div>
+                <div id="welcomeProblemsTitle">
+                    {problem.Title}
+                </div>
               </div>
-            </div>
-          </Link>
-      </li>
+            
+        </li>
+      </Link>
 
       );
    }
