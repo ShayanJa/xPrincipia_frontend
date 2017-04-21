334c698c1bdd5a3fea2e154429b1fbe9f4d7b93d
ShayanJa <shay.talebi@gmail.com>
2017-04-20 22:27:10 -0500

    voting functionality for questions

--- src/components/QuestionUnit.jsx
+++ src/components/QuestionUnit.jsx
@@ -4,21 +4,14 @@ import axios from 'axios';
 import cookie from 'react-cookie';
 
 export default class QuestionUnit extends React.Component {
+constructor(props){
+        super(props);
 
-  submitVote() {
-       axios.post('http://localhost:10000/auth/vote/create', {
-           Type: 1,
-           TypeID: this.state.questionInfo.ID,
-           username : cookie.load("userName"),
-           
-        })
-        .then(function (result) {
-            document.location = window.location.pathname 
-        })
-        .catch(function (error) {
-            alert("You've already voted on a question.")
-        })
-  }
+        this.renderItem = this.renderItem.bind(this)
+        // this.submitVote = this.submitVote.bind(this)
+
+    };
+  
 
 	render() {
 		return (
@@ -29,6 +22,20 @@ export default class QuestionUnit extends React.Component {
 		);
 	}
 	renderItem(question) {
+       function submitVote() {
+       axios.post('http://localhost:10000/auth/vote/create', {
+           Type: 2,
+           TypeID: question.ID,
+           username : cookie.load("userName"),
+           
+        })
+        .then(function (result) {
+            document.location = window.location.pathname 
+        })
+        .catch(function (error) {
+            alert("You've already voted on a question.")
+        })
+    }
   
     return (
         <li key={question.ID} id="questionUnit"> 
@@ -36,7 +43,7 @@ export default class QuestionUnit extends React.Component {
 					<div id="questionAdder">Q: {question.Username}</div>
                 	<div id="questionText">{question.Description}</div>
 				</div>
-				<button type="button" id="questionVote">Vote<br />{floatToDecimal(question.PercentRank)}</button>
+				<button type="button" id="questionVote" onClick={submitVote}>Vote<br />{floatToDecimal(question.PercentRank)}</button>
             <Link to={`/problem/${question.TypeID}/question/${question.ID}/answers`} activeClassName="activeBlue"><button type="button" id="questionAnswers">Answers</button></Link>
         <br/><br/> 
         </li>)
