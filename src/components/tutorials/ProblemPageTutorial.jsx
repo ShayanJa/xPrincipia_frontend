64c5efc11c56f99c9a34044f744830e02156a405
bfrance18@gmail.com <bfrance18gmailcom@bfrance18gmailcoms-MacBook-Pro.local>
2017-05-31 13:47:22 -0500

    added css to problem tutorial

--- src/components/tutorials/ProblemPageTutorial.jsx
+++ src/components/tutorials/ProblemPageTutorial.jsx
@@ -4,25 +4,34 @@ import { Link  } from 'react-router';
 export default class ProblemPageTutorial extends React.Component {
    render() {
       return (
-      <div id="tutorialProblemBackground">
         <div id="tutorialProblem">
           <div id="tutorialbanner">
               <div id="tutorialTitle">Problem Page Tutorial</div>
-              <div id="tutorialSummary">Break down and solve problems</div>
+              <div id="tutorialSummary">
+                  <span id="tutorialNumbers">1. </span>
+                  Break down and solve problems
+              </div>
           </div>
           <div id="tutorialProse">
-                  .
-                  <br /> Problems are broken down into sub problem trees
+                Problems are broken down into sub problem trees
 
-                 <br />.<br />At each level solutions are created
-
-                  <br />.<br />Discuss the problem: 
+                 <br />At each level solutions are created<br /><br />
+              <div id="tutorialSummary">
+                  <span id="tutorialNumbers">2. </span>
+                    Discuss the problem: 
+              </div>
                   <br />Ask or answer questions
                   <br />Suggest new ideas <br />
                   Engage in freeform discussion
                   <br /> 
-                  <br />Learn and teach the skills required for 
-                  further problem solving
+                  <br />
+              <div id="tutorialSummary">
+                  <span id="tutorialNumbers">2. </span>
+                    Learn and teach:
+              </div>
+                   {/*the skills required for 
+                  further problem solving*/}
+                  
               <br />
           </div>
           {/*<Link to={`/problem/${this.state.parentID}/subproblems`}>*/}
@@ -31,7 +40,6 @@ export default class ProblemPageTutorial extends React.Component {
             </div>
           {/*</Link>*/}
         </div>
-      </div>
       );
    }
 }
