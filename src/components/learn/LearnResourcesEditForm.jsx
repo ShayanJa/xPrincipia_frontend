2a8f668f742f092d1587e4f91e2f02277a94c7ec
ShayanJa <shay.talebi@gmail.com>
2017-07-01 11:26:07 -0500

    fix resource edit form

--- src/components/learn/LearnResourcesEditForm.jsx
+++ src/components/learn/LearnResourcesEditForm.jsx
@@ -17,7 +17,7 @@ export default class LearnResourcesEditForm extends React.Component {
 
   componentWillMount(){
       var self = this; ///TODO Change Resouces to resources when backend Changes
-        return axios.get('http://ec2-13-58-239-116.us-east-2.compute.amazonaws.com/auth/resouces/ID?id='+this.props.params.resourceID).then(function (response) {
+        return axios.get('http://ec2-13-58-239-116.us-east-2.compute.amazonaws.com/auth/resources/ID?id='+this.props.params.resourceID).then(function (response) {
           self.setState({
               resource: response.data
           })
@@ -37,7 +37,7 @@ updateResource() {
 
   axios.put('http://ec2-13-58-239-116.us-east-2.compute.amazonaws.com/auth/resources/update?id='+this.props.params.resourceID, {
       type:'0',
-      typeID: this.props.probID,
+      typeID: this.props.params.probID,
       username: cookie.load('userName'),
       description : this.state.resource,
     })
