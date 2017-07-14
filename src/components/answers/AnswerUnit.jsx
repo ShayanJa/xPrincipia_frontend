a5cd528cdc72899595877de8669d67dcc80c9e7d
ShayanJa <shay.talebi@gmail.com>
2017-07-14 15:43:30 -0500

    change vote to unvote when users vote on answers

--- src/components/answers/AnswerUnit.jsx
+++ src/components/answers/AnswerUnit.jsx
@@ -13,17 +13,43 @@ constructor(props){
         // this.submitVote = this.submitVote.bind(this)
 
     };
+
+    componentDidMount() {
+
+    }
+
+    componentWillReceiveProps (props) {
+        var self = this
+        console.log('hey')
+        console.log(props.answers[1].ID)
+        self.setState({
+            voteArray : []
+        })
+        for (var i=0; i < props.answers.length; i++){
+            axios.get( Config.API + "/auth/vote/isVotedOn?type=4&typeID=" + props.answers[i].ID + "&username=" + cookie.load("userName"))
+            .then( function (response){
+                self.setState({
+                voteArray : self.state.voteArray.concat(response.data)
+            })
+            
+            
+        })
+        }
+      
+    
+    }
+    
 	render() {
 		return (
 	    <div>
-			<ul> {this.props.answers.map(this.renderItem)} </ul>
+			<ul> {this.props.answers.map(this.renderItem, this)} </ul>
 	               
 	    </div>
 		);
 	}
 
    renderItem(answer) {
-       function submitVote() {
+    function submitVote() {
        axios.post( Config.API + '/auth/vote/create', {
            Type: 4,
            TypeID: answer.ID,
@@ -37,7 +63,21 @@ constructor(props){
             alert("You may vote for only one answer per question.")
         })
     }
-    if (answer.Username === cookie.load('userName')) {
+
+    function isVotedOn (typeID, username) {
+        var self = this
+        axios.get( Config.API + "/auth/vote/isVotedOn?type=4&typeID=1&username=Shyshawn")
+            .then( function (response){
+                // console.log(response.data)
+                return response.data
+            })
+        }
+    
+
+
+
+    
+    if (this.state.voteArray[answer.ID] === true && answer.Username === cookie.load('userName')) {
         return (
         <li key={answer.ID} id="answerUnit">
 				<div id="answerContent">
@@ -60,11 +100,40 @@ constructor(props){
                     </div>
                 </Link>
             <button type="button" onClick={submitVote} id="suggestionVote">
+                UnVote
+            </button>
+        </li>);
+
+    }  else if ( answer.Username === cookie.load('userName')) {
+        return (
+        <li key={answer.ID} id="answerUnit">
+				<div id="answerContent">
+					<div id="discussHeaderGreen">
+                        <span id="discussPercent">{floatToDecimal(answer.PercentRank)}</span>
+					    {answer.Username}
+                        
+                    </div>
+                    <div id="suggestionText">
+                        {answer.Description}
+                    </div>
+				</div>
+                <Link to={`/problem/${this.props.probID}/question/${this.props.questID}/answer/${answer.TypeID}/delete`}>
+                    <div id="deleteSBButton">
+                        <img src={require('../../assets/delete.svg')} id="editLogo" width="18" height="18" alt="Delete Button" />
+                    </div>
+                </Link>
+                <Link to={`/problem/${this.props.probID}/question/${this.props.questID}/answer/${answer.ID}/edit`}>
+                    <div id="editSBButtonAnswer">
+                        <img src={require('../../assets/editBlue.svg')} id="editLogo" width="18" height="18" alt="Edit Button" />
+                    </div>
+                </Link>
+            <button type="button" onClick={submitVote} id="suggestionVote">
                 Vote
             </button>
         </li>);
 
-    } else {
+    } 
+    else {
     return (
         <li key={answer.ID} id="answerUnit">
 				<div id="answerContent">
@@ -76,6 +145,7 @@ constructor(props){
                         {answer.Description}
                     </div>
 				</div>
+
             <button type="button" onClick={submitVote} id="suggestionVoteNoComments">
                 Vote
             </button>
