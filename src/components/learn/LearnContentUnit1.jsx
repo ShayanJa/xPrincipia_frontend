ed18288a070199b3bcf9a9389f36d2ffe4361064
bfrance18@gmail.com <bfrance18gmailcom@bfrance18gmailcoms-MacBook-Pro.local>
2017-07-15 17:37:19 -0500

    voting and unvoting works for learn lessons

--- src/components/learn/LearnContentUnit1.jsx
+++ src/components/learn/LearnContentUnit1.jsx
@@ -11,6 +11,24 @@ export default class LearnContentUnit1 extends React.Component {
          this.renderItem = this.renderItem.bind(this)
     };
 
+    componentWillReceiveProps (props) {
+        var self = this
+        self.setState({
+            voteHash : {},
+        })
+        props.learnItems.forEach( function (learnItem){
+            axios.get( Config.API + "/auth/vote/isVotedOn?type=7&typeID=" + learnItem.ID + "&username=" + cookie.load("userName"))
+            .then( function (response) {  
+                const voteHash = self.state.voteHash;
+
+                voteHash[learnItem.ID] = response.data
+                self.setState({
+                    voteHash,
+                })
+            })  
+        })
+    }
+
     render() {
 		return (
             <div>
@@ -35,9 +53,58 @@ export default class LearnContentUnit1 extends React.Component {
             alert("I'm sorry, you've already voted on a comment.");
         })
   }
+      function unVote() {
+      axios.delete( Config.API + '/auth/vote/delete' ,{
+        params: {
+          type: 7,
+          typeID: learnItem.ID,
+          username: cookie.load('userName')
+        }
+        })
+        .then(function (result) {
+            document.location = window.location.pathname 
+        })
+        .catch(function (error) {
+            alert("I'm sorry, there was a problem with your request. ")
+        })
+        
+    }
 
-       if (learnItem.Username === cookie.load('userName')) {
+       if (this.state.voteHash[learnItem.ID] === true && learnItem.Username === cookie.load('userName')) {
            return (
+       <li key={learnItem.ID} id="suggestionUnit">
+				<div id="suggestionContent">
+					<div id="discussHeader">
+                        <span id="discussPercent">{floatToDecimal(learnItem.PercentRank)}</span>
+					    {learnItem.Username}
+                    </div>
+                    <div id="learnContentText">
+                        {learnItem.Description}
+                    </div>
+				</div>
+                <Link to={`/problem/${learnItem.TypeID}/learn/content/${learnItem.ID}/delete`}>
+                   <div id="deleteSBButton">
+                        <img src={require('../../assets/delete.svg')} id="editLogo" width="18" height="18" alt="Delete Button" />
+                    </div>
+                </Link>
+                <Link to={`/problem/${learnItem.TypeID}/learn/content/${learnItem.ID}/edit`}>
+                    <div id="editSBButtonAnswer">
+                        <img src={require('../../assets/editBlue.svg')} id="editLogo" width="18" height="18" alt="Edit Button" />
+                    </div>
+                </Link>
+                {/*<Link to={`/problem/${learnItem.TypeID}/learn/content/${learnItem.ID}/comments`}>
+                    <div id="commentSBButtonUser">
+                            <img src={require('../../assets/comments.svg')} id="commentLogo" width="24" height="24" alt="Comments Button" />
+                    </div>                
+                </Link>*/}
+            	<button type="button" onClick={unVote} id="suggestionVoted">
+                    Voted
+                </button> 
+                <br /><br /> 
+        </li>);
+
+    }  else if ( learnItem.Username === cookie.load('userName')) {
+        return (
        <li key={learnItem.ID} id="suggestionUnit">
 				<div id="suggestionContent">
 					<div id="discussHeader">
@@ -68,7 +135,34 @@ export default class LearnContentUnit1 extends React.Component {
                 </button> 
                 <br /><br /> 
         </li>);
-
+    } else if (this.state.voteHash[learnItem.ID] === true) {
+        return (
+        <li key={learnItem.ID} id="questionUnit"> 
+				<div id="suggestionContent">
+					<div id="discussHeader">
+                        <span id="discussPercent">{floatToDecimal(learnItem.PercentRank)}</span>
+					    {learnItem.Username}
+                    </div>
+                    <div id="suggestionText">
+                        {learnItem.Description}
+                    </div>
+				</div>
+                    {/*<Link to={`/problem/${learnItem.TypeID}/learn/contentItem/${learnItem.ID}/flag`}>
+                        <div id="flagSBButton">
+                            <img src={require('.../src/assets/flag.svg')} id="deleteLogo" width="11" height="11" alt="Delete Button, Red X" />
+                            Flag
+                        </div>
+                    </Link>*/}
+                {/*<Link to={`/problem/${learnItem.TypeID}/learn/content/${learnItem.ID}/comments`} activeClassName="activeBlue">
+                    <div id="commentSBButtonUser">
+                            <img src={require('../../assets/comments.svg')} id="commentLogo" width="24" height="24" alt="Comments Button" />
+                    </div>                
+                </Link>*/}
+                <button type="button" id="suggestionVotedNoComments" onClick={unVote}>
+                    Voted
+                </button>
+                <br/><br/> 
+        </li>);
   } else {
     return (
         <li key={learnItem.ID} id="questionUnit"> 
