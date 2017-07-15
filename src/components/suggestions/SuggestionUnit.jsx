157fc77219b75abb6a431cfa894a9013125e0227
bfrance18@gmail.com <bfrance18gmailcom@bfrance18gmailcoms-MacBook-Pro.local>
2017-07-15 15:58:27 -0500

    voting and unvoting working on suggestions

--- src/components/suggestions/SuggestionUnit.jsx
+++ src/components/suggestions/SuggestionUnit.jsx
@@ -5,12 +5,30 @@ import cookie from 'react-cookie';
 import {Config} from '../../config.js'
 
 export default class SuggestionUnit extends React.Component {
+    
     constructor(props){
         super(props);
+        
          this.renderItem = this.renderItem.bind(this)
     };
   
+    componentWillReceiveProps (props) {
+        var self = this
+        self.setState({
+            voteHash : {},
+        })
+        props.suggestions.forEach( function (suggestion){
+            axios.get( Config.API + "/auth/vote/isVotedOn?type=3&typeID=" + suggestion.ID + "&username=" + cookie.load("userName"))
+            .then( function (response) {  
+                const voteHash = self.state.voteHash;
 
+                voteHash[suggestion.ID] = response.data
+                self.setState({
+                    voteHash,
+                })
+            })  
+        })
+    }
 	render() {
 		return (
 	    <div>
@@ -34,10 +52,56 @@ export default class SuggestionUnit extends React.Component {
         .catch(function (error) {
             alert("You may only vote on a suggestion once.");
         })
+       }
+        function unVote() {
+        axios.delete( Config.API + '/auth/vote/delete' ,{
+            params: {
+            type: 3,
+            typeID: suggestion.ID,
+            username: cookie.load('userName')
+        }
+        })
+        .then(function (result) {
+            document.location = window.location.pathname 
+        })
+        .catch(function (error) {
+            alert("I'm sorry, there was a problem with your request. ")
+        })
   }
   
-       if (suggestion.Username === cookie.load('userName')) {
+       if (this.state.voteHash[suggestion.ID] === true && suggestion.Username === cookie.load('userName')) {
            return (
+       <li key={suggestion.ID} id="suggestionUnit">
+				<div id="suggestionContent">
+					<div id="discussHeader">
+                        <span id="discussPercent">{floatToDecimal(suggestion.PercentRank)}</span>
+					    {suggestion.Username}
+                    </div>
+                    <div id="suggestionText">
+                        {suggestion.Description}
+                    </div>
+				</div>
+                <Link to={`/problem/${suggestion.TypeID}/suggestion/${suggestion.ID}/delete`}>
+                    <div id="deleteSBButton">
+                        <img src={require('../../assets/delete.svg')} id="editLogo" width="18" height="18" alt="Delete Button" />
+                    </div>
+                </Link>
+                <Link to={`/problem/${suggestion.TypeID}/suggestion/${suggestion.ID}/edit`}>
+                    <div id="editSBButton">
+                        <img src={require('../../assets/editBlue.svg')} id="editLogo" width="18" height="18" alt="Edit Button" />
+                    </div>
+                </Link>
+                <Link  to={`/problem/${suggestion.TypeID}/suggestion/${suggestion.ID}/comments`} activeClassName="activeBlue">
+                    <div id="commentSBButtonUser">
+                            <img src={require('../../assets/comments.svg')} id="commentLogo" width="24" height="24" alt="Comments Button" />
+                    </div>
+                </Link>
+                <button type="button" onClick={unVote} id="suggestionVoted">
+                    Voted
+                </button>  
+        </li>);
+    }  else if ( suggestion.Username === cookie.load('userName')) {
+        return (
        <li key={suggestion.ID} id="suggestionUnit">
 				<div id="suggestionContent">
 					<div id="discussHeader">
@@ -68,7 +132,34 @@ export default class SuggestionUnit extends React.Component {
                 </button> 
                 <br /><br /> 
         </li>);
-
+    } else if (this.state.voteHash[suggestion.ID] === true) {
+        return (
+           <li key={suggestion.ID} id="suggestionUnit">
+				<div id="suggestionContent">
+					<div id="discussHeader">
+                        <span id="discussPercent">{floatToDecimal(suggestion.PercentRank)}</span>
+					    {suggestion.Username}
+                    </div>
+                    <div id="suggestionText">
+                        {suggestion.Description}
+                    </div>
+				</div>
+                    {/*<Link to={`/problem/${suggestion.TypeID}/suggestion/${suggestion.ID}/flag`}>
+                        <div id="flagSBButton">
+                            <img src={require('.../src/assets/flag.svg')} id="deleteLogo" width="11" height="11" alt="Delete Button, Red X" />
+                            Flag
+                        </div>
+                    </Link>*/}
+                <Link  to={`/problem/${suggestion.TypeID}/suggestion/${suggestion.ID}/comments`} activeClassName="activeBlue">
+                    <div id="commentSBButtonUser">
+                            <img src={require('../../assets/comments.svg')} id="commentLogo" width="24" height="24" alt="Comments Button" />
+                    </div>
+                </Link>
+                <button type="button" onClick={unVote} id="suggestionVoted">
+                    Voted
+                </button> 
+                <br /><br /> 
+        </li>);
     } else {
     return (
        <li key={suggestion.ID} id="suggestionUnit">
