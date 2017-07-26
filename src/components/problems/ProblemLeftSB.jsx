28f7adb947d9091ba95e3e36d8162ce21021a068
ShayanJa <shay.talebi@gmail.com>
2017-07-25 20:49:03 -0500

    add vote and unvoted to problemleft side , however voted styles are wierd

--- src/components/problems/ProblemLeftSB.jsx
+++ src/components/problems/ProblemLeftSB.jsx
@@ -12,61 +12,54 @@ export default class ProblemLeftSB extends React.Component {
 
         this.state = {
             problemInfo: [],
+            parentInfo: [],
         }
         this.submitVote = this.submitVote.bind(this)
         this.unVote = this.unVote.bind(this)
     };
-    componentWillMount(){
+    componentDidMount(){
       var self = this;
-      return axios.get( Config.API + '/auth/problems/ID?id='+this.props.params.probID).then(function (response) {
-          //if parent ID is 0 then the problem is at the root of the tree
-          // return id as the parentID for routing purposes
-          if (response.data.ParentID === 0){
-            self.setState({
-              parentID: response.data.ID
-            })
-          }
-          //set other data
+      axios.get( Config.API + '/auth/problems/ID?id='+this.props.params.probID).then(function (response) {
+
+          //set Problem Data
           self.setState({
               problemInfo: response.data
           })
-    }).then( function(response){
-
     })
     .catch(function (error) {
-        // if(error.response.status === 401 || error.response.status === 403){
-        //     document.location = "/login"
-        // }
-    });   
+        if(error.response.status === 401 || error.response.status === 403){
+            document.location = "/login"
+        }
+    });
+          
+    axios.get( Config.API + "/auth/vote/isVotedOn?type=0&typeID=" + this.props.params.probID + "&username=" + cookie.load("userName"))
+          .then( function (response){
+            console.log(response.data)
+            self.setState({
+              vote: response.data
+            })
+      })       
   }
+
+
   componentWillReceiveProps(newProps){
     var self = this;
       return axios.get( Config.API + '/auth/problems/ID?id='+newProps.params.probID).then(function (response) {
-          //if parent ID is 0 then the problem is at the root of the tree
-          // return id as the parentID for routing purposes
-          if (response.data.ParentID === 0){
-            self.setState({
-              parentID: response.data.ID
-            })
-          }
-          //set other data
-          self.setState({
-              problemInfo: response.data,
-              probID: response.data.ID
-          })
-    }).then( function(response){
-      
+        //set problem data
+        self.setState({
+            problemInfo: response.data,
+            probID: response.data.ID
+        })
     })
     .catch(function (error) {
-        // if(error.response.status === 401 || error.response.status === 403){
-        //     document.location = "/login"
-        // }
+        if(error.response.status === 401 || error.response.status === 403){
+            document.location = "/login"
+        }
     }); 
 
   }
   submitVote() {
-      // Warning on console says self is not necessary here, commenting it out to be safe
-      // var self = this
+      var self = this
        axios.post( Config.API + '/auth/vote/create', {
            Type: 0,
            TypeID: this.state.problemInfo.ID,
@@ -74,22 +67,36 @@ export default class ProblemLeftSB extends React.Component {
            
         })
         .then(function (result) {
-            document.location = window.location.pathname;
-            alert("Thank you, your vote has been recorded.")
+            // alert("Thank you, your vote has been recorded.")
+          return axios.get( Config.API + '/auth/problems/ID?id='+self.props.params.probID).then(function (response) {
+          
+            //set problem data
+            self.setState({
+                problemInfo: response.data,
+                // vote: true,
+            })
+            document.location = window.location.pathname 
+          })
+          
         })
         .catch(function (error) {
             alert("You may only vote on a project once. ");
         })
   }
- unVote() {
-      axios.delete( Config.API + '/auth/vote/delete' ,{
+unVote() {
+      return axios.delete( Config.API + '/auth/vote/delete' ,{
         params: {
           type: 0,
-          typeID: this.state.problemInfo.ID,
+          typeID: this.props.params.probID,
           username: cookie.load('userName')
         }
         })
         .then(function (result) {
+            //set problem data
+            // self.setState({
+                
+            //     vote: false,
+            // })
             document.location = window.location.pathname 
         })
         .catch(function (error) {
@@ -100,6 +107,151 @@ export default class ProblemLeftSB extends React.Component {
 
    render() {
 
+       if (this.state.vote ===true && this.state.problemInfo.OriginalPosterUsername === cookie.load('userName')) {
+           return (
+      <div id="fullWide">
+          <ReactCSSTransitionGroup
+          transitionName="example"
+          transitionAppear={true}
+          transitionAppearTimeout={2000}
+          transitionEnter={false}
+          transitionLeave={false}>
+        <div id="maxContainerRow">
+          <div id="fullProblemLeft">
+            <div id="problemLeftSBHeader">
+              <Link to={`/problem/${this.props.params.probID}/subproblems`}>
+                <div id="backProjectArrowDiv">
+                  <img src={require('../../assets/leftArrow.svg')} id="backArrowBlueHover" width="50" height="30" alt="Back arrow, blue up arrow" />
+                </div>
+              </Link>
+            </div>
+            <div id="problemLeftSBIntro">
+              <div id="SPHeaderLeftSB">
+                <div id="SPTitleLeftSB">{this.state.problemInfo.Title}</div>
+                <div id="SPPercentLeftSB">
+                  <span id="bigPercent">{floatToDecimal(this.state.problemInfo.PercentRank)}</span>%
+                </div>
+              </div> 
+                <div id="fullProblemHeaderSB">
+                  <div id="problemAdditionalInfoLabelSB">Additional Info</div>
+                  <div id="votedProjectLeftSB" onClick={this.unVote}>Voted</div>
+                </div>
+                <p id="problemSummaryLeftSB">
+                  {this.state.problemInfo.Summary}
+                </p>
+              </div>
+            </div>
+          <div id="sidebar">
+            {React.cloneElement(this.props.children, {probID: this.state.probID})}
+          </div>
+        </div>
+
+          {/*<div id="tutorialSolutionButtonDiv">
+            <img src={require('../../assets/tutorial.svg')} id="tutorialSolutionButton" width="50" height="50" alt="Back arrow, blue up arrow" />
+          </div>*/}
+          
+          <TutorialSolutionPage />
+          </ReactCSSTransitionGroup>
+      </div>
+      );
+       } else if(this.state.problemInfo.OriginalPosterUsername === cookie.load('userName')) {
+           return (
+      <div id="fullWide">
+          <ReactCSSTransitionGroup
+          transitionName="example"
+          transitionAppear={true}
+          transitionAppearTimeout={2000}
+          transitionEnter={false}
+          transitionLeave={false}>
+        <div id="maxContainerRow">
+          <div id="fullProblemLeft">
+            <div id="problemLeftSBHeader">
+              <Link to={`/problem/${this.props.params.probID}/subproblems`}>
+                <div id="backProjectArrowDiv">
+                  <img src={require('../../assets/leftArrow.svg')} id="backArrowBlueHover" width="50" height="30" alt="Back arrow, blue up arrow" />
+                </div>
+              </Link>
+            </div>
+            <div id="problemLeftSBIntro">
+              <div id="SPHeaderLeftSB">
+                <div id="SPTitleLeftSB">{this.state.problemInfo.Title}</div>
+                <div id="SPPercentLeftSB">
+                  <span id="bigPercent">{floatToDecimal(this.state.problemInfo.PercentRank)}</span>%
+                </div>
+              </div> 
+                <div id="fullProblemHeaderSB">
+                  <div id="problemAdditionalInfoLabelSB">Additional Info</div>
+                  <div id="followProblemLeftSB" onClick={this.submitVote}>Vote</div>
+                </div>
+                <p id="problemSummaryLeftSB">
+                  {this.state.problemInfo.Summary}
+                </p>
+              </div>
+            </div>
+          <div id="sidebar">
+            {React.cloneElement(this.props.children, {probID: this.state.probID})}
+          </div>
+        </div>
+
+          {/*<div id="tutorialSolutionButtonDiv">
+            <img src={require('../../assets/tutorial.svg')} id="tutorialSolutionButton" width="50" height="50" alt="Back arrow, blue up arrow" />
+          </div>*/}
+          
+          <TutorialSolutionPage />
+          </ReactCSSTransitionGroup>
+      </div>
+             );
+       } else if(this.state.vote === true) {
+         return (      
+         <div id="fullWide">
+          <ReactCSSTransitionGroup
+          transitionName="example"
+          transitionAppear={true}
+          transitionAppearTimeout={2000}
+          transitionEnter={false}
+          transitionLeave={false}>
+        <div id="maxContainerRow">
+          <div id="fullProblemLeft">
+            <div id="problemLeftSBHeader">
+              <Link to={`/problem/${this.props.params.probID}/subproblems`}>
+                <div id="backProjectArrowDiv">
+                  <img src={require('../../assets/leftArrow.svg')} id="backArrowBlueHover" width="50" height="30" alt="Back arrow, blue up arrow" />
+                </div>
+              </Link>
+            </div>
+            <div id="problemLeftSBIntro">
+              <div id="SPHeaderLeftSB">
+                <div id="SPTitleLeftSB">{this.state.problemInfo.Title}</div>
+                <div id="SPPercentLeftSB">
+                  <span id="bigPercent">{floatToDecimal(this.state.problemInfo.PercentRank)}</span>%
+                </div>
+              </div> 
+                <div id="fullProblemHeaderSB">
+                  <div id="problemAdditionalInfoLabelSB">Additional Info</div>
+                  <div id="votedProjectLeftSB" onClick={this.unVote}>Voted</div>
+                </div>
+                <p id="problemSummaryLeftSB">
+                  {this.state.problemInfo.Summary}
+                </p>
+              </div>
+            </div>
+          <div id="sidebar">
+            {React.cloneElement(this.props.children, {probID: this.state.probID})}
+          </div>
+        </div>
+
+          {/*<div id="tutorialSolutionButtonDiv">
+            <img src={require('../../assets/tutorial.svg')} id="tutorialSolutionButton" width="50" height="50" alt="Back arrow, blue up arrow" />
+          </div>*/}
+          
+          <TutorialSolutionPage />
+          </ReactCSSTransitionGroup>
+      </div>
+      );
+
+       }
+
+    else {
       return (
       <div id="fullWide">
           <ReactCSSTransitionGroup
@@ -145,7 +297,8 @@ export default class ProblemLeftSB extends React.Component {
           <TutorialSolutionPage />
           </ReactCSSTransitionGroup>
       </div>
-      );
+              );
+   }
    }
 }
 
