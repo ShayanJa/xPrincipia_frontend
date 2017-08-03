791828eed4913fd0c05218fb9b82eaa88f611e6b
ShayanJa <shay.talebi@gmail.com>
2017-08-03 15:21:51 -0500

    remove stuff

--- src/config.js
+++ src/config.js
@@ -3,7 +3,4 @@ export class Config {
     static API =  'http://ec2-13-58-239-116.us-east-2.compute.amazonaws.com'
     //LocalHostAPI
     // static API = 'http://localhost:10000'
-    //productionAPI
-    // static API = 'http://ec2-52-15-178-109.us-east-2.compute.amazonaws.com'
-
 }
